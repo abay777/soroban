fed756d2c8a2b37eee93cd8a10fdb2bb04a0c7af
abaysankar <abaysankarbmdigitalutilisation@gmail.com>
2024-11-21 16:18:14 +0530

    "updated the design and added responsiveness"

--- src/components/abacus/Abacus.jsx
+++ src/components/abacus/Abacus.jsx
@@ -12,11 +12,11 @@ const AbacusRow = ({ beads, rowIndex, isFirstRow, onClick }) => {
   const colBeads = 13; // Number of beads in the row
 
   return (
-    <div className="flex items-center space-x-10 py-2 mx-auto">
+    <div className="flex items-center md:space-x-5 space-x-3 lg:space-x-7 lg:py-1 mx-auto  ">
       {[...Array(colBeads)].map((_, index) => (
         <div
           key={index}
-          className="w-5 h-5 rounded-full bg-custom-radial"
+          className={`md:w-7 md:h-7  w-3 h-3 bead ${isFirstRow?'mb-1':''}`}
           style={{
             // Apply vertical movement based on bead's position
             transform: `translateY(${beads[index]}px)`,
@@ -40,7 +40,7 @@ AbacusRow.propTypes = {
 const Abacus = ({setResult}) => {
   // Initial state setup: an array of rows with each bead position (in pixels)
   const [rows, setRows] = useState([
-    new Array(13).fill(-20), // Special row, initial bead position is 30px (default)
+    new Array(13).fill(-10), // Special row, initial bead position is 30px (default)
     new Array(13).fill(0), // Regular row
     new Array(13).fill(0), // Regular row
     new Array(13).fill(0), // Regular row
@@ -61,11 +61,11 @@ const Abacus = ({setResult}) => {
   
     if (isFirstRow) {
       // Handle first row bead movement
-      if (newRow[rowIndex][beadIndex] === -20) {
+      if (newRow[rowIndex][beadIndex] === -10) {
         newRow[rowIndex][beadIndex] = 0; // Move bead down
         tempValue += beadValue[0][beadIndex] *5;
       } else if (newRow[rowIndex][beadIndex] === 0) {
-        newRow[rowIndex][beadIndex] = -20; // Move bead up
+        newRow[rowIndex][beadIndex] = -10; // Move bead up
         tempValue -= beadValue[0][beadIndex] *5;
       }
     } else {
@@ -75,15 +75,15 @@ const Abacus = ({setResult}) => {
         // Moving bead up
         for (let i = rowIndex; i >= 1; i--) {
           if (newRow[i][beadIndex] === 0) {
-            newRow[i][beadIndex] = -20;
+            newRow[i][beadIndex] = -10;
             tempValue += beadValue[0][beadIndex]; // Accumulate value  
          
           }
         }
-      } else if (newRow[rowIndex][beadIndex] === -20) {
+      } else if (newRow[rowIndex][beadIndex] === -10) {
         // Moving bead down
         for (let i = rowIndex; i <= newRow.length - 1; i++) {
-          if (newRow[i][beadIndex] === -20) {
+          if (newRow[i][beadIndex] === -10) {
             newRow[i][beadIndex] = 0;
             console.log(rowIndex , beadIndex ,'row & bead')
             tempValue -= beadValue[0][beadIndex]; // Subtract value
@@ -97,8 +97,9 @@ const Abacus = ({setResult}) => {
     setRows(newRow);
   };
   return (
-    <div className="flex flex-col items-start space-y-2 p-4 max-w-min bg-white mx-auto px-10 py-5 mt-2 absolute rounded-md border-black border-[1px]" style={{ zIndex: 100 }}>
+    <div className="abacusBox min-w-[350px] flex flex-col items-center md:items-start space-y-2 p-4  bg-white mx-auto lg:px-10 py-5 mt-5 absolute rounded-md border-black border-[1px]" style={{ zIndex: 100 }}>
       {/* Render all rows */}
+      <hr className="h-1 w-[92%] rounded-[2px] -translate-x-1/2 left-1/2 top-[26%] bg-gradient-to-tr from-gray-300 from-100% via-gray-200 via-100%  to-gray-700 to-100% absolute " style={{zIndex:130}} />
       {rows.map((row, index) => (
         <AbacusRow
           key={index}
